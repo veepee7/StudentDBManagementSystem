a88fd0903f231024d7b30098140e39d97a42dc81
viljooo7 <sarma.villepetteri@gmail.com>
2021-03-09 13:58:31 +0200

    Color update to lila/violet

--- dbmanagement_frontend.py
+++ dbmanagement_frontend.py
@@ -10,7 +10,7 @@ class Student:
         self.root = root
         self.root.title("Student Database Management System")
         self.root.geometry("1350x750+0+0")
-        self.root.config(bg="cadet blue")
+        self.root.config(bg="#db46f0")
 
         StdID = StringVar()
         Firstname = StringVar()
@@ -95,66 +95,66 @@ class Student:
 
 
         #=========================== Frames =============================#
-        MainFrame = Frame(self.root, bg="cadet blue")
+        MainFrame = Frame(self.root, bg="#db46f0")
         MainFrame.grid()
 
-        TitleFrame = Frame(MainFrame, bd=2, padx=54, pady=8, bg="ghost white", relief=RIDGE)
+        TitleFrame = Frame(MainFrame, bd=2, padx=54, pady=8, bg="#eedefd", relief=RIDGE)
         TitleFrame.pack(side=TOP)
 
-        self.lblTitle = Label(TitleFrame, font=('arial', 40, 'bold'), text="Student Database Management Systems", bg="ghost white")
+        self.lblTitle = Label(TitleFrame, font=('arial', 40, 'bold'), text="Student Database Management Systems", bg="#eedefd")
         self.lblTitle.grid(sticky=W)
 
-        ButtonFrame = Frame(MainFrame, bd=2, width=1200, height=70, padx=18, pady=10, bg="ghost white", relief=RIDGE)
+        ButtonFrame = Frame(MainFrame, bd=2, width=1200, height=70, padx=18, pady=10, bg="#eedefd", relief=RIDGE)
         ButtonFrame.pack(side=BOTTOM)
 
-        DataFrame = Frame(MainFrame, bd=1, width=950, height=400, padx=20, pady=20, bg="cadet blue", relief=RIDGE)
+        DataFrame = Frame(MainFrame, bd=1, width=950, height=400, padx=20, pady=20, bg="#db46f0", relief=RIDGE)
         DataFrame.pack(side=BOTTOM)
 
-        DataFrameLEFT = LabelFrame(DataFrame, bd=1, width=350, height=600, padx=20, bg="ghost white", relief=RIDGE, 
+        DataFrameLEFT = LabelFrame(DataFrame, bd=1, width=350, height=600, padx=20, bg="#eedefd", relief=RIDGE, 
                                 font=('arial', 20, 'bold'), text="Student Info\n")
         DataFrameLEFT.pack(side=LEFT)
 
-        DataFrameRIGHT = LabelFrame(DataFrame, bd=1, width=300, height=300, padx=31, pady=3, bg="ghost white", relief=RIDGE,
+        DataFrameRIGHT = LabelFrame(DataFrame, bd=1, width=300, height=300, padx=31, pady=3, bg="#eedefd", relief=RIDGE,
                                 font=('arial', 20, 'bold'), text="Student Details\n")
         DataFrameRIGHT.pack(side=RIGHT)
 
         #=========================== Labels and entry widget =============================#
-        self.lblStdID = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Student ID:", padx=2, pady=2, bg="ghost white")
+        self.lblStdID = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Student ID:", padx=2, pady=2, bg="#eedefd")
         self.lblStdID.grid(row=0, column=0, sticky=W)
         self.txtStdID = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=StdID, width=39)
         self.txtStdID.grid(row=0, column=1)
 
-        self.lblFna = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="First name:", padx=2, pady=2, bg="ghost white")
+        self.lblFna = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="First name:", padx=2, pady=2, bg="#eedefd")
         self.lblFna.grid(row=1, column=0, sticky=W)
         self.txtFna = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Firstname, width=39)
         self.txtFna.grid(row=1, column=1)
 
-        self.lblSna = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Surname:", padx=2, pady=2, bg="ghost white")
+        self.lblSna = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Surname:", padx=2, pady=2, bg="#eedefd")
         self.lblSna.grid(row=2, column=0, sticky=W)
         self.txtSna = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Surname, width=39)
         self.txtSna.grid(row=2, column=1)
 
-        self.lblDoB = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Date of birth:", padx=2, pady=3, bg="ghost white")
+        self.lblDoB = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Date of birth:", padx=2, pady=3, bg="#eedefd")
         self.lblDoB.grid(row=3, column=0, sticky=W)
         self.txtDoB = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=DoB, width=39)
         self.txtDoB.grid(row=3, column=1)
 
-        self.lblAge = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Age:", padx=2, pady=3, bg="ghost white")
+        self.lblAge = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Age:", padx=2, pady=3, bg="#eedefd")
         self.lblAge.grid(row=4, column=0, sticky=W)
         self.txtAge = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Age, width=39)
         self.txtAge.grid(row=4, column=1)
 
-        self.lblGender = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Gender:", padx=2, pady=3, bg="ghost white")
+        self.lblGender = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Gender:", padx=2, pady=3, bg="#eedefd")
         self.lblGender.grid(row=5, column=0, sticky=W)
         self.txtGender = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Gender, width=39)
         self.txtGender.grid(row=5, column=1)
 
-        self.lblAdr = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Address:", padx=2, pady=3, bg="ghost white")
+        self.lblAdr = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Address:", padx=2, pady=3, bg="#eedefd")
         self.lblAdr.grid(row=6, column=0, sticky=W)
         self.txtAdr = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Address, width=39)
         self.txtAdr.grid(row=6, column=1)
 
-        self.lblMobile = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Mobile:", padx=2, pady=3, bg="ghost white")
+        self.lblMobile = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Mobile:", padx=2, pady=3, bg="#eedefd")
         self.lblMobile.grid(row=7, column=0, sticky=W)
         self.txtMobile = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Mobile, width=39)
         self.txtMobile.grid(row=7, column=1)
